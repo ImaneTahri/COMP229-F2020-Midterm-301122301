f8145b52fc2b91f5c7a2cc0db1e081a7ce888bb4
Imane Tahri <itahri@my.centennialcollege.ca>
2020-10-30 15:30:45 -0400

    submission

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb://userlocal:qwerty12345@clusterstuff-shard-00-02-nkcve.mongodb.net:27017/books229?ssl=true&replicaSet=ClusterStuff-shard-0&authSource=admin&retryWrites=true"
+};
+
